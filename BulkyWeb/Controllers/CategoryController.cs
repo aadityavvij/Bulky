528225517ed0da3d28a76cdd2eae3a35220f3fa9
Aaditya <aadityavardhanvij.bt22cse@pec.edu.in>
2024-01-04 20:20:53 +0530

    edit database from ui

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Controllers
 {
@@ -36,5 +37,71 @@ namespace BulkyWeb.Controllers
 				return View(obj);
 			}
 		}
+		public IActionResult Edit(int? id)
+		{
+			if(id==null || id == 0)
+			{
+				return View(id);
+			}
+			Category? cat = _db.Category.Find(id);
+			if(cat == null)
+			{
+				return View(id);
+			}
+			return View(cat);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("Name", "The display order cannot be exact same as the Name");
+			}
+			if (ModelState.IsValid)
+			{
+				_db.Category.Update(obj);
+				_db.SaveChanges();
+				TempData["SuccessMessage"] = "Field Edited Successfully!";
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View(obj);
+			}
+		}
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return View(id);
+			}
+			Category? cat = _db.Category.Find(id);
+			if (cat == null)
+			{
+				return View(id);
+			}
+			return View(cat);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePost(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+
+			Category? cat = _db.Category.Find(id);
+
+			if (cat == null)
+			{
+				return NotFound();
+			}
+
+			_db.Category.Remove(cat);
+			_db.SaveChanges();
+			TempData["DeleteMessage"] = "Field Deleted Successfully!";
+			return RedirectToAction("Index");
+		}
+
 	}
 }
