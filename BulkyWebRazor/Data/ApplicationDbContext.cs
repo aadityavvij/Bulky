aa34eb0a4ece3c9773112dbfe3fbd85c5e1d6de5
Aaditya <aadityavardhanvij.bt22cse@pec.edu.in>
2024-01-05 13:18:33 +0530

    razorPages

---
+++ BulkyWebRazor/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using BulkyWebRazor.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace BulkyWebRazor.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+		base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Category>().HasData(
+				new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+				new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+				new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+				);
+		}
+
+		public DbSet<Category> Category { get; set; }
+	}
+}
