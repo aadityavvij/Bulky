2e29375da670d8b3ffd5512f1b9faebc4ff0ea29
Aaditya <aadityavardhanvij.bt22cse@pec.edu.in>
2024-01-06 20:19:22 +0530

    n_tier

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,11 +1,11 @@
-﻿using BulkyWeb.Data;
-using BulkyWeb.Models;
+﻿using Bulky.DataAccess.Data;
+using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Controllers
 {
-    public class CategoryController(ApplicationDbContext db) : Controller
+	public class CategoryController(ApplicationDbContext db) : Controller
     {
         private readonly ApplicationDbContext _db = db;
 
